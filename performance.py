1caffd2ef1254e2bf9e2f682df9584e58cc204d1
mathfunction <mathfunction@gmail.com>
2019-10-09 18:37:32 +0800

    performance code

--- performance.py
+++ performance.py
@@ -24,13 +24,17 @@ def performance(_boolOpenVINO=False):
 	
 	data = ReadMINST(False)
 	n_train = data.xtrain.size(0)
+	n_test = data.xtest.size(0)
 	#=========== start to run ====================#
 	if _boolOpenVINO == False:
 		on_pure_pytorch = InferenceEngine()
 		t1 = timeit.default_timer()
 		for i in range(n_train):
 			x = data.xtrain[i]
-			on_pure_pytorch.infer(x.view(1,x.size(0),x.size(1))) 
+			on_pure_pytorch.infer(x.view(1,x.size(0),x.size(1)))
+		for i in range(n_test):
+			x = data.xtest[i]
+			on_pure_pytorch.infer(x.view(1,x.size(0),x.size(1)))  
 		t2 = timeit.default_timer()
 
 		print("[{}],time_cost:{}s".format("pure_pytorch",t2-t1))
@@ -39,6 +43,9 @@ def performance(_boolOpenVINO=False):
 		t1 = timeit.default_timer()
 		for i in range(n_train):
 			x = data.xtrain[i]
+			on_openvino.infer(x.numpy().reshape(1,28,28))
+		for i in range(n_test):
+			x = data.xtest[i]
 			on_openvino.infer(x.numpy().reshape(1,28,28))
 		t2 = timeit.default_timer()
 		print("[{}],time_cost:{}s".format("openvino_pytorch",t2-t1))
